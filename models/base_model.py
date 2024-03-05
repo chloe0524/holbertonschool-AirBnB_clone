35855407ae54733417b26dc5a712e41703caf5d0
mathpvx <7547@holbertonstudents.com>
2024-03-05 11:07:27 +0100

    added comments for more clarity of the code

--- models/base_model.py
+++ models/base_model.py
@@ -10,9 +10,9 @@ class BaseModel:
 
     def __init__(self):
         """Initialize a BaseModel instance."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        self.id = str(uuid.uuid4())  # generate unique ID for the instance
+        self.created_at = datetime.now()  # set the creation timestamp
+        self.updated_at = datetime.now()  # set the update timestamp
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""
@@ -25,8 +25,8 @@ class BaseModel:
 
     def to_dict(self):
         """Return a dictionary representation of the BaseModel instance."""
-        data = self.__dict__.copy()
-        data['__class__'] = self.__class__.__name__
-        data['created_at'] = self.created_at.isoformat()
-        data['updated_at'] = self.updated_at.isoformat()
+        data = self.__dict__.copy()  # make a copy of the instance dictrionnary
+        data['__class__'] = self.__class__.__name__  # class name
+        data['created_at'] = self.created_at.isoformat()  # conversion to ISO
+        data['updated_at'] = self.updated_at.isoformat()  # converts to ISO
         return data

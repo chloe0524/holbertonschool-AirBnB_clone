9945568f9ec4c4424269c6a285b4a1658b45c478
mathpvx <7547@holbertonstudents.com>
2024-03-05 22:29:33 +0100

    reload and init on base

--- models/base_model.py
+++ models/base_model.py
@@ -22,14 +22,15 @@ class BaseModel:
         if kwargs:
             for key, val in kwargs.items():
                 if key != '__class__':
+                    setattr(self, key, val)
                     if key in ['created_at', 'update_at']:
                         val = datetime.strptime(val, "%Y-%m-%dT%H:%M:%S.%f")
-                setattr(self, key, val)
-            storage.new(self)
+                        setattr(self, key, val)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """Return a string representation of the BaseModel instance."""

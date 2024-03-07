5cbdf136c858d54c25469b7e1c8b8a22039989d7
chloe0524 <chloco31@gmail.com>
2024-03-07 12:20:32 +0100

    changed save test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,56 +1,52 @@
 #!/usr/bin/python3
-"""'unittests' for 'BaseModel class'"""
-
+"""Module for Base Model unittests."""
 import uuid
 import unittest
 import datetime
+
 from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    """'unittests' for BaseModel class"""
-
+    """Tests Base Model class."""
     def test_save(self):
         """Test save()"""
-        new_mod = BaseModel()
-        new_mod.save()
-        self.assertNotEqual(new_mod.created_at, new_mod.updated_at)
+        my_model = BaseModel()
+        my_model.save()
+        self.assertNotEqual(my_model.created_at, my_model.updated_at)
 
-    def test_save_helped(self):
-        """Test save(self)"""
-        new_mod = BaseModel()
-        new_mod.save()
-        self.assertIsInstance(new_mod.updated_at, datetime.datetime)
-
-    def test_created_at(self):
-        """Test created_at"""
-        base_model = BaseModel()
-        self.assertIsInstance(base_model.created_at, datetime.datetime)
-
-    def test_updated_at(self):
-        """Test updated_at"""
-        base_model = BaseModel()
-        self.assertIsInstance(base_model.updated_at, datetime.datetime)
-
-    def test_id(self):
-        """Test id"""
-        base_model = BaseModel()
-        self.assertIsInstance(base_model.id, str)
+    def test_save2(self):
+        my_model = BaseModel()
+        my_model.save()
+        expected = "BaseModel." + my_model.id
+        with open("file.json", "r") as f:
+            self.assertIn(expected, f.read())
 
     def test_to_dict(self):
-        """Test to_dict """
-        new_mod = BaseModel()
-        new_mod.name = "This is a model (to_dict test)"
-        new_mod_json = new_mod.to_dict()
-        self.assertEqual(new_mod_json["id"], new_mod.id)
+        """Test to_dict()"""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.number = 89
+        my_model_json = my_model.to_dict()
+        self.assertEqual(my_model_json["id"], my_model.id)
+        self.assertEqual(my_model_json["created_at"],
+                         my_model.created_at.isoformat())
 
-    def test___str__(self):
-        """Test __str__"""
-        new_mod = BaseModel()
-        new_mod.name = "This is a model (__str__ test)"
-        self.assertEqual(str(new_mod),
-                         "[BaseModel] ({}) {}".format
-                         (new_mod.id, new_mod.__dict__))
+    def test__str__(self):
+        """Test __str__()"""
+        my_model = BaseModel()
+        my_model.name = "My First Model"
+        my_model.number = 89
+        self.assertEqual(str(my_model),
+                         f"[BaseModel] ({my_model.id}) {my_model.__dict__}")
+
+    def test_init(self):
+        """Test __init__()"""
+        my_model = BaseModel()
+        self.assertIsInstance(my_model, BaseModel)
+        self.assertIsInstance(my_model.id, str)
+        self.assertIsInstance(my_model.created_at, datetime.datetime)
+        self.assertIsInstance(my_model.updated_at, datetime.datetime)
 
 
 if __name__ == "__main__":

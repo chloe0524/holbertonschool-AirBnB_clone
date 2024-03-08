954f4d69450936af927b3b02752a2c7d2038a1f1
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-03-08 13:15:15 +0100

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,26 +1,41 @@
 #!/usr/bin/python3
-"""
-Module for City unittest
-"""
+""" unittests for state class """
+
 import uuid
 import json
-from models.base_model import BaseModel
-from models.state import State
 import unittest
-from datetime import datetime
+
 from models import storage
+from models.state import State
+from datetime import datetime
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage(unittest.TestCase):
-    """
-    Test the State_id class attr
-    """
-    def test_user_state(self):
+class test_state(unittest.TestCase):
+
+    def test_to_dict(self):
+        state = State()
+        state_dict = state.to_dict()
+
+        self.assertIsInstance(state_dict, dict)
+        self.assertEqual(state_dict["__class__"], "State")
+        self.assertEqual(state_dict["id"], state.id)
+        self.assertEqual(state_dict["created_at"],
+                         state.created_at.isoformat())
+        self.assertEqual(state_dict["updated_at"],
+                         state.updated_at.isoformat())
+        self.assertEqual(state_dict["name"], state.name)
+
+    def test_name(self):
+        state = State()
+        state.name = "Ohio"
+        self.assertEqual(state.name, "Ohio")
+
+    def test_name_empty(self):
         state = State()
         self.assertEqual(state.name, "")
 
-    def test_user_city_1(self):
+    def test_state_of_base_model(self):
         state = State()
-        state.name = "France"
-        self.assertEqual(state.name, "France")
+        self.assertIsInstance(state, BaseModel)

c15c007dfcb3c7764e7b3d97cc980a4972fd2f08
chloe0524 <127857895+chloe0524@users.noreply.github.com>
2024-03-08 13:19:20 +0100

    Update test_review.py fix  tests issues

--- tests/test_models/test_review.py
+++ tests/test_models/test_review.py
@@ -1,24 +1,40 @@
 #!/usr/bin/python3
 """unittest for Review class"""
+
 import uuid
 import json
-from models.base_model import BaseModel
-from models.review import Review
 import unittest
-from datetime import datetime
+
 from models import storage
+from models.review import Review
+from datetime import datetime
+from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 
 
-class TestFileStorage(unittest.TestCase):
-    """
-    Test the State_id class attr
-    """
-    def test_user_review(self):
-        review = Review()
-        self.assertEqual(review.place_id, "")
+class test_review(unittest.TestCase):
 
-    def test_user_review_1(self):
+    def test_to_dict(self):
         review = Review()
-        review.place_id = "Paris"
-        self.assertEqual(review.place_id, "Paris")
+        review_dict = review.to_dict()
+
+        self.assertIsInstance(review_dict, dict)
+        self.assertEqual(review_dict["__class__"], "Review")
+        self.assertEqual(review_dict["id"], review.id)
+        self.assertEqual(review_dict["created_at"],
+                         review.created_at.isoformat())
+        self.assertEqual(review_dict["updated_at"],
+                         review.updated_at.isoformat())
+        self.assertEqual(review_dict["text"], review.text)
+        self.assertEqual(review_dict["user_id"], review.user_id)
+        self.assertEqual(review_dict["place_id"], review.place_id)
+
+    def test_text(self):
+        review = Review()
+        review.text = "owlbear cub sumpremacy"
+        self.assertEqual(review.text, "owlbear cub sumpremacy")
+
+    def test_user_id(self):
+        review = Review()
+        review.user_id = "5652"
+        self.assertEqual(review.user_id, "5652")

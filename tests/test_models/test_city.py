722b9d4a10995622e0f8d62577f1bc157de6b673
mathpvx <7547@holbertonstudents.com>
2024-03-08 11:15:31 +0100

    unittest city

---
+++ tests/test_models/test_city.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+import uuid
+import json
+import unittest
+
+from models import storage
+from models.city import City
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestCity(unittest.TestCase):
+
+    def test_name(self):
+        city = City()
+        city.name = (
+            "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"
+        )
+        self.assertEqual(
+            city.name,
+            "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"
+        )
+
+    def test_state_id(self):
+        city = City()
+        city.state_id = "1234"
+        self.assertEqual(city.state_id, "1234")

be0915f9cef1fd58dc9f38046f4fd3422539ed32
chloe0524 <chloco31@gmail.com>
2024-03-08 12:14:02 +0100

    added a to_disct test to verify serialization

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -12,6 +12,19 @@ from models.engine.file_storage import FileStorage
 
 class tests_amenity(unittest.TestCase):
 
+    def test_to_dict(self):
+        amenity = Amenity()
+        amenity_dict = amenity.to_dict()
+
+        self.assertIsInstance(amenity_dict, dict)
+        self.assertEqual(amenity_dict["__class__"], "Amenity")
+        self.assertEqual(amenity_dict["id"], amenity.id)
+        self.assertEqual(amenity_dict["created_at"],
+                         amenity.created_at.isoformat())
+        self.assertEqual(amenity_dict["updated_at"],
+                         amenity.updated_at.isoformat())
+        self.assertEqual(amenity_dict["name"], amenity.name)
+
     def test_name(self):
         """Test name attribute"""
         amenity = Amenity()
